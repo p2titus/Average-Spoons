97773259773f5fc6ea84ce66b619f6a7afc8f366
mike <michael.hallam3@gmail.com>
2021-09-06 13:34:22 +0100

    now return average of coordinates

--- locationaverage/averager.py
+++ locationaverage/averager.py
@@ -3,12 +3,14 @@ import geopy
 from geopy import Location, Nominatim
 import time
 
+Coordinate = (float, float)
+
+
 class Addr:
     house: str
     line1: str
     line2: str
     postcode: str
-    coords: (float, float)
 
 
 def __default_locator_gen():
@@ -21,15 +23,21 @@ delay_s is to ensure apis aren't queried too frequently
 """
 def average_locations(locs: [Addr],
                       generate_locator=__default_locator_gen,
-                      delay_s=1) -> Addr:
+                      delay_s=1) -> Coordinate:
     locator = generate_locator()
     coords = map(lambda loc: __loc_to_coords(loc, locator), locs)
+    n = len(locs)
     latacc, longacc = 0, 0
 
     for coord in coords:
         latacc += coord.latitude
         longacc += coord.longitude
         time.sleep(delay_s)
+
+    if n > 0:
+        return latacc/n, longacc/n
+    else:
+        return None
 
 
 

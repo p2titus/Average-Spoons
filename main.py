d31225ae7c584b0b6f9bd8c270b5088da8114a5f
mike <michael.hallam3@gmail.com>
2021-09-06 20:35:56 +0100

    added basic main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def find_closest_spoons(addr):
+    import locationservices
+    avg = locationservices.average_locations(addr)
+    pub = locationservices.get_nearby(avg)
+    print(pub)
+
 
 if __name__ == '__main__':
-    print('nothing here yet')
+    addrs = None
+    find_closest_spoons(addrs)

70cb78365b04641f15885028dff58d57091583b3
mike <michael.hallam3@gmail.com>
2021-09-08 14:35:00 +0100

    added small test data to main

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ def find_closest_spoons(addr):
     print(pub)
 
 
+def __gen_buck_palace():
+    from locationservices.averager import Addr
+    a = Addr()
+    a.house = 'Buckingham Palace'
+    a.line2 = 'London'
+    a.postcode = 'SW1A 1AA'
+
+
 if __name__ == '__main__':
     addrs = None
     find_closest_spoons(addrs)

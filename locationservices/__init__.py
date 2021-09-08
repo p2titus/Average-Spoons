64865c9b5e1d88d2b498273f748e6db37010a058
mike <michael.hallam3@gmail.com>
2021-09-08 14:41:06 +0100

    fixed init to reflect fixes to circular import

--- locationservices/__init__.py
+++ locationservices/__init__.py
@@ -1,2 +1,3 @@
-from .averager import Coordinate, Address, average_locations
+from .averager import average_locations
+from .containers import Coordinate, Addr
 from .locator import get_nearby, translate_coords_addr

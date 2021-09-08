9020babbc710562ba99e4de88bfc2a2b85203760
mike <michael.hallam3@gmail.com>
2021-09-08 14:26:58 +0100

    added small fix - mports from same package must be prefixed with a .

--- locationservices/__init__.py
+++ locationservices/__init__.py
@@ -1,2 +1,2 @@
-from averager import Coordinate, Address, average_locations
-from locator import get_nearby, translate_coords_addr
+from .averager import Coordinate, Address, average_locations
+from .locator import get_nearby, translate_coords_addr
